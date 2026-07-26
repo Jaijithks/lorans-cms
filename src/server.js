68f9dee3a86dc4a9b145eb23b4fdc9c28e57e39a
jaijithks <kathirjas20@gmail.com>
2026-07-27 00:59:36 +0530

    changes

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ app.use((req, res, next) => {
     'http://127.0.0.1:5173',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://lorans.vercel.app'
   ];
   const origin = req.headers.origin;
 
